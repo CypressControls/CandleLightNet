6b3536f0869036430702d043d861363eb28d1ad4
Tylor Pater <tylor@cypressrobotics.ca>
2019-07-11 16:05:45 -0700

    Add license

--- src/Resource.h
+++ src/Resource.h
@@ -1,3 +1,7 @@
+//This source is released under the LGPL V3
+//Copyright (C) 2019 Cypress Robotics Corporation. 
+//Portions of this code are taken from https://github.com/HubertD/candle_dll
+
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by app.rc
--- src/gs_usb.h
+++ src/gs_usb.h
@@ -1,2 +1,5 @@
+//This source is released under the LGPL V3
+//Copyright (C) 2019 Cypress Robotics Corporation. 
+//Portions of this code are taken from https://github.com/HubertD/candle_dll
 #pragma once
 
